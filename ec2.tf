dfd718bbf37686ce463b2dbeeaaed0b6f79442d9
chierighini <chierighini.andre@gmail.com>
2022-10-29 17:49:01 -0300

    Add bot instance and key pair

--- ec2.tf
+++ ec2.tf
@@ -11,3 +11,17 @@ resource "aws_instance" "jenkins-instance" {
     Name = "Jenkins-Instance"
   }
 }
+
+resource "aws_isntance" "discord-bot-instance" {
+  ami           = "ami-0f72d3c899140e51a"
+  instance_type = "t2.nano"
+  #vpc_security_group_ids      = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
+  subnet_id                   = aws_subnet.main_public.id
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.ssh-key-discord-bot.key_name
+  #user_data                   = file("install_jenkins.sh")
+
+  tags = {
+    Name = "Jenkins-Instance"
+  }
+}
--- kms.tf
+++ kms.tf
@@ -2,3 +2,8 @@ resource "aws_key_pair" "ssh-key-jenkins" {
   key_name   = "ssh-key-jenkins"
   public_key = var.jenkins-public-key
 }
+
+resource "aws_key_pair" "ssh-key-discord-bot" {
+  key_name   = "ssh-key-discord-bot"
+  public_key = var.jenkins-public-key
+}
