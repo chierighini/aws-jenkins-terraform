2cd2fe038a8ffa40db24797edbe32a1f99e51c69
chierighini <chierighini.andre@gmail.com>
2022-10-29 18:22:27 -0300

    Add backend bucket

---
+++ s3.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "backend-bucket" {
+  bucket = "unique-bucket-name-0"
+  tags = {
+    Name    = "Bucket 0"
+    BotHost = "Yes"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket-0-acl" {
+  bucket = aws_s3_bucket.backend-bucket.id
+  acl    = "private"
+}
