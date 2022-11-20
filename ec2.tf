1064257a61fa6d3e7b17bc5546d1951ddfb00e36
chierighini <chierighini.andre@gmail.com>
2022-11-20 20:30:15 -0300

    add module variables

--- ec2.tf
+++ ec2.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "jenkins-instance" {
   ami                         = "ami-0f72d3c899140e51a"
   instance_type               = "t3.micro"
   vpc_security_group_ids      = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
-  subnet_id                   = aws_subnet.main_public.id
+  subnet_id                   = var.subnet_id
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.general_ssh_key.key_name
+  key_name                    = var.ssh_key_name
   user_data                   = file("install_jenkins.sh")
 
   tags = {
--- sg.tf
+++ sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
   description = "Allow SSH and Jenkins inbound traffic"
-  vpc_id      = aws_vpc.main_vpc.id
+  vpc_id      = var.vpc_id
 
   ingress {
     from_port   = 22
